08d4d1db11aefe24671f2f73afb3b48c7d2e0402
bekyiu <651731457@qq.com>
2019-04-10 15:08:22 +0800

    Page77

---
+++ src/other/chapter2/Page77.java
@@ -0,0 +1,68 @@
+package other.chapter2;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Page77
+{
+	public static void main(String[] args)
+	{
+		
+	}
+	class ListNode
+	{
+		int val;
+		ListNode next;
+
+		ListNode(int x)
+		{
+			val = x;
+		}
+	}
+	//hash 时间复杂度On 空间复杂度On
+	public void removeRep1(ListNode head)
+	{
+		ListNode cur = head.next;
+		ListNode pre = head;
+		Set<Integer> set = new HashSet<>();
+		set.add(head.val);
+		while(cur != null)
+		{
+			if(set.contains(cur.val))
+			{
+				pre.next = cur.next;
+			}
+			else
+			{
+				set.add(cur.val);
+				pre = cur;
+			}
+			cur = cur.next;
+		}
+	}
+	//时间复杂度n^2, 空间复杂度O1
+	public void removeRep2(ListNode head)
+	{
+		ListNode cur = head;
+		ListNode next = null;
+		ListNode pre = null;
+		while(cur != null)
+		{
+			pre = cur;
+			next = cur.next;
+			while(next != null)
+			{
+				if(next.val == cur.val)
+				{
+					pre.next = cur.next;
+				}
+				else
+				{
+					pre = next;
+				}
+				next = next.next;
+			}
+			cur = cur.next;
+		}
+	}
+}
