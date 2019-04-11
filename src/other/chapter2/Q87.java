8b767875f89c24b3fdb76e2d302aae7529c4d7bf
bekyiu <651731457@qq.com>
2019-04-11 14:37:12 +0800

    Page87

---
+++ src/other/chapter2/Q87.java
@@ -0,0 +1,51 @@
+package other.chapter2;
+
+public class Q87
+{
+	class ListNode
+	{
+		int val;
+		ListNode next;
+
+		ListNode(int x)
+		{
+			val = x;
+		}
+	}
+	
+	public ListNode insertNum(ListNode head, int num)
+	{
+		ListNode node = new ListNode(num);
+		if(head == null)
+		{
+			node.next = node;
+			return node;
+		}
+		ListNode pre = head;
+		ListNode cur = head.next;
+		boolean flag = false;
+		while(cur != head)
+		{
+			if(pre.val <= num && num <= cur.val)
+			{
+				flag = true;
+				pre.next = node;
+				node.next = cur;
+			}
+			pre = cur;
+			cur = cur.next;
+		}
+		if(!flag)
+		{
+			//此时cur指向head
+			pre.next = node;
+			node.next = cur;
+			if(num <= cur.val)
+			{
+				return node;
+			}
+		}
+		return head;
+		
+	}
+}
