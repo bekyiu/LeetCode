6ed237b459af4483cc6e541d5c8751af9883d1ef
bekyiu <651731457@qq.com>
2019-08-09 11:32:22 +0800

    Page314 Q132

---
+++ src/hard/Q132.java
@@ -0,0 +1,29 @@
+package hard;
+// Page314
+public class Q132
+{
+    public int minCut(String s) {
+        if(s == null || s.equals(""))
+    	{
+    		return 0;
+    	}
+    	int len = s.length();
+    	int[] dp = new int[len + 1];
+    	dp[len] = -1;
+    	boolean[][] p = new boolean[len][len];
+    	char[] chs = s.toCharArray();
+    	for(int i = len - 1; i >= 0; i--)
+    	{
+    		dp[i] = Integer.MAX_VALUE;
+    		for(int j = i; j < len; j++)
+    		{
+    			if(chs[i] == chs[j] && (j - i < 2 || p[i + 1][j - 1]))
+    			{
+    				p[i][j] = true;
+    				dp[i] = Math.min(dp[i], dp[j + 1] + 1);
+    			}
+    		}
+    	}
+    	return dp[0];
+    }
+}
