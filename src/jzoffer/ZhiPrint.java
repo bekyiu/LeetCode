998dc444ca378a1a7d53e2142c46aa742b2768a8
bekyiu <651731457@qq.com>
2019-08-23 12:33:25 +0800

    剑指offer 之字打印二叉树

---
+++ src/jzoffer/ZhiPrint.java
@@ -0,0 +1,67 @@
+package jzoffer;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Deque;
+
+class TreeNode
+{
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode(int x)
+	{
+		val = x;
+	}
+}
+
+public class ZhiPrint
+{
+	public static void main(String[] args)
+	{
+	}
+	public ArrayList<ArrayList<Integer>> print(TreeNode root)
+	{
+		ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+		if (root == null)
+		{
+			return list;
+		}
+		Deque<TreeNode> queue = new ArrayDeque<>();
+		queue.add(root);
+		int flag = 1;
+		while (!queue.isEmpty())
+		{
+			ArrayList<Integer> level = new ArrayList<>();
+			int count = queue.size();
+			while (count > 0)
+			{
+				TreeNode cur = queue.poll();
+				level.add(cur.val);
+				if (cur.left != null)
+				{
+					queue.add(cur.left);
+				}
+				if (cur.right != null)
+				{
+					queue.add(cur.right);
+				}
+				count--;
+			}
+			// 奇数 从左向右打印, 偶数从右向左打印
+			if((flag & 1) == 1)
+			{
+				list.add(level);
+			}
+			else
+			{
+				Collections.reverse(level);
+				list.add(level);
+			}
+			flag++;
+		}
+		return list;
+	}
+}
