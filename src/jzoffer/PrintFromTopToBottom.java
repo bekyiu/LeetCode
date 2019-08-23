056a54e3def3e9f1c32a01723778bb42206411f8
bekyiu <651731457@qq.com>
2019-08-23 12:17:28 +0800

    剑指offer 层序遍历 分层打印

--- src/jzoffer/PrintFromTopToBottom.java
+++ src/jzoffer/PrintFromTopToBottom.java
@@ -17,29 +17,62 @@ public class PrintFromTopToBottom
 			val = x;
 		}
 	}
-	
-    public ArrayList<Integer> printFromTopToBottom(TreeNode root) 
-    {
-    	ArrayList<Integer> list = new ArrayList<>();
-    	if(root == null)
-    	{
-    		return list;
-    	}
-    	Deque<TreeNode> queue = new ArrayDeque<>();
-    	queue.add(root);
-    	while(!queue.isEmpty())
-    	{
-    		TreeNode cur = queue.poll();
-    		list.add(cur.val);
-    		if(cur.left != null)
-    		{
-    			queue.add(cur.left);
-    		}
-    		if(cur.right != null)
-    		{
-    			queue.add(cur.right);
-    		}
-    	}
-    	return list;
-    }
+
+	public ArrayList<Integer> printFromTopToBottom(TreeNode root)
+	{
+		ArrayList<Integer> list = new ArrayList<>();
+		if (root == null)
+		{
+			return list;
+		}
+		Deque<TreeNode> queue = new ArrayDeque<>();
+		queue.add(root);
+		while (!queue.isEmpty())
+		{
+			TreeNode cur = queue.poll();
+			list.add(cur.val);
+			if (cur.left != null)
+			{
+				queue.add(cur.left);
+			}
+			if (cur.right != null)
+			{
+				queue.add(cur.right);
+			}
+		}
+		return list;
+	}
+
+	// 分行打印
+	public ArrayList<ArrayList<Integer>> print(TreeNode root)
+	{
+		ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+		if (root == null)
+		{
+			return list;
+		}
+		Deque<TreeNode> queue = new ArrayDeque<>();
+		queue.add(root);
+		while (!queue.isEmpty())
+		{
+			ArrayList<Integer> level = new ArrayList<>();
+			int count = queue.size();
+			while(count > 0)
+			{
+				TreeNode cur = queue.poll();
+				level.add(cur.val);
+				if (cur.left != null)
+				{
+					queue.add(cur.left);
+				}
+				if (cur.right != null)
+				{
+					queue.add(cur.right);
+				}
+				count--;
+			}
+			list.add(level);
+		}
+		return list;
+	}
 }
